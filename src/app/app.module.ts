54f224c101e69c82914ac1fddad7d85ab95e78d4
Silviu Barbu <s.barbu@goit.global>
2025-05-21 09:44:58 +0300

    Added send mesage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
@@ -18,13 +18,10 @@ import { ModalComponent } from './modal/modal.component';
     CoursesComponent,
     AboutComponent,
     FooterComponent,
-    ModalComponent
+    ModalComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/modal/modal.component.ts
+++ src/app/modal/modal.component.ts
@@ -1,4 +1,6 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import emailjs from '@emailjs/browser';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-modal',
@@ -9,14 +11,53 @@ export class ModalComponent {
   @Input() courseTitle: string = '';
   @Output() close = new EventEmitter<void>();
 
+  formData = {
+    name: '',
+    email: '',
+    phone: '',
+  };
+
+  isSubmitting = false;
+
   closeModal() {
     this.close.emit();
   }
 
-  submitForm() {
-    // Handle form submission logic here
-    console.log('Form submitted for course:', this.courseTitle);
-    // You might want to close the modal after submission
-    this.closeModal();
+  submitForm(form: NgForm) {
+    if (form.invalid) {
+      // Marchează toate câmpurile ca fiind atinse pentru a afișa erorile
+      Object.keys(form.controls).forEach((key) => {
+        form.controls[key].markAsTouched();
+      });
+      return; // Nu trimitem dacă formularul este invalid
+    }
+
+    this.isSubmitting = true; // Activăm starea de încărcare
+
+    const templateParams = {
+      course: this.courseTitle,
+      name: this.formData.name,
+      email: this.formData.email,
+      phone: this.formData.phone,
+    };
+
+    console.log('Sending data:', templateParams);
+
+    emailjs
+      .send(
+        'service_xd827el',
+        'template_wc5657i',
+        templateParams,
+        'user_EelcXR1O2dtBCcKFTmS2V'
+      )
+      .then((res) => {
+        console.log('Email sent successfully:', res.status, res.text);
+        this.isSubmitting = false; // Dezactivăm starea de încărcare
+        this.closeModal();
+      })
+      .catch((err) => {
+        console.error('Email sending failed:', err);
+        this.isSubmitting = false; // Dezactivăm starea de încărcare și în caz de eroare
+      });
   }
 }
